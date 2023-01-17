5b4e13f83ed86f4feea1088112e03bfde9c05a54
Eliyahu Levy <Eliyahu.Levy21@gmail.com>
2023-01-17 11:54:53 -0500

    better fake lookup

--- price_lookup.py
+++ price_lookup.py
@@ -10,6 +10,9 @@ import time
 from os import path, listdir
 from os.path import isfile, join
 from collections import defaultdict
+import math
+import random
+import pandas as pd
 
 class Yahoo_Price_Lookup():
     def __init__(self, cache_dir):
@@ -21,7 +24,7 @@ class Yahoo_Price_Lookup():
             if isfile(join(cache_dir, f)):
                 parsed_filename = f.split('.')[0]
                 data = pd.read_csv(f"{cache_dir}/{f}", index_col=0)
-                self.lookup_table[ticker] = data
+                self.lookup_table[parsed_filename] = data
 
         self.blacklist = set()
         self.blacklist.add("NLSN")
@@ -36,25 +39,28 @@ class Yahoo_Price_Lookup():
             return None
         if ticker in self.lookup_table and self.lookup_table[ticker] is None:
             return None
-        if ticker in self.lookup_table and date in self.lookup_table[ticker] and self.lookup_table[ticker][date] == None:
-            return None
         if ticker in self.blacklist:
             return None
         
-        if ticker not in self.lookup_table or str(date) not in self.lookup_table[ticker]:
+        if ticker in self.lookup_table:
+            result = self.lookup_table[ticker][self.lookup_table[ticker]["date"] == str(date)]
+        else:
+            result = []
+
+        if ticker not in self.lookup_table or len(result) == 0:
             # print(f"Cache miss on {ticker} {str(date)}\n{self.lookup_table[ticker]}")
             new_data = self.get_thirty_days_prices(ticker, date, date + datetime.timedelta(days=30))
             self.lookup_table[ticker] = pd.concat([self.lookup_table[ticker], new_data]).reset_index(drop=True)
             self.lookup_table[ticker].to_csv(f"{self.cache_dir}/{ticker}.csv")
-            result = self.lookup_table[ticker][self.lookup_table[ticker]["date"] == date]
+            result = self.lookup_table[ticker][self.lookup_table[ticker]["date"] == str(date)]
             if len(result) == 0:
                 # print(f"Twas not in lookup table. {self.lookup_table.values()}")
-                self.lookup_table[ticker].loc[len(self.lookup_table[ticker])] = [date, 0, 0, 0]
+                self.lookup_table[ticker].loc[len(self.lookup_table[ticker])] = [str(date), 0, 0, "0"]
                 return None
 
 
-        result = self.lookup_table[ticker][self.lookup_table[ticker]["date"] == date]
-        if result["timestamp"] == 0:
+        result = result.iloc[0]
+        if result["timestamp"] == "0":
             return None
 
         price = (result["open"] + result["close"]) / 2
@@ -85,8 +91,8 @@ class Yahoo_Price_Lookup():
             date = datetime.datetime.fromtimestamp(timestamps[i])
             price_open = price_data["open"][i],
             price_close = price_data["close"][i],
-            timestamp = date.timestamp()
-            result.loc[len(result)] = [datetime.datetime(date.year, date.month, date.day), price_open, price_close, timestamp]
+            timestamp = str(date.timestamp())
+            result.loc[len(result)] = [str(datetime.datetime(date.year, date.month, date.day)), price_open, price_close, timestamp]
 
         # print(f"Price result: {result}")
         return result
@@ -94,11 +100,39 @@ class Yahoo_Price_Lookup():
     def load_tickers(self, tickers, start_date, end_date):
         for ticker in tickers:
             print(f"fetching ticker {ticker}")
-            new_data = self.get_thirty_days_prices(ticker, start_date, end_date=end_date)
+            new_data = self.get_thirty_days_prices(ticker, start_date, end_date)
             self.lookup_table[ticker] = pd.concat([self.lookup_table[ticker], new_data]).reset_index(drop=True)
             self.lookup_table[ticker].to_csv(f"{self.cache_dir}/{ticker}.csv")
 
 
-class Fake_Price_Lookup():
-    def lookup(self, ticker, date):
-        return 10
+class Fake_Price_Lookup(Yahoo_Price_Lookup):
+
+    def __init__(self, cache_dir):
+        super().__init__(cache_dir)
+
+    def get_thirty_days_prices(self, ticker, start_date, end_date):
+        result = pd.DataFrame(columns=["date", "open", "close", "timestamp"])
+
+        curr_day = start_date
+        while curr_day < end_date:
+            open_price = random.random() * 500 + 100
+            result.loc[len(result)] = [str(curr_day), open_price, open_price + random.random() * 10 - 5, str(curr_day.timestamp())]
+            curr_day += datetime.timedelta(days=1)
+        return result
+
+
+def main():
+    price_lookup = Fake_Price_Lookup("cached_ticker_data")
+    date = datetime.datetime(2023, 1, 18)
+    print(f"GOOG: {price_lookup.lookup('GOOG', date)}")
+    print(f"AAPL: {price_lookup.lookup('AAPL', date)}")
+    print(f"OJAD: {price_lookup.lookup('OJAD', date)}")
+    print(f"ADAW: {price_lookup.lookup('ADAW', date)}")
+
+    print(f"GOOG: {price_lookup.lookup('GOOG', datetime.datetime(2022, 1, 18))}")
+
+
+if __name__ == "__main__":
+    main()
+
+
